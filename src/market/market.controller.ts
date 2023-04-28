4245a2abdf0ec5da06dca6a104a6878b26b91b35
bbattulga <battulga.dev@gmail.com>
2023-04-29 01:55:52 +0800

    update /market

--- src/market/market.controller.ts
+++ src/market/market.controller.ts
@@ -22,16 +22,28 @@ export class MarketController {
             display_symbol: string,
             price: Prisma.Decimal,
             prev_price: Prisma.Decimal,
-        }[]>`select
-        a.symbol as "symbol",
-        a.display_symbol,
-        a."name" as "name",
-        (select p.price from asset_prices p where p.base_symbol = a.symbol and p.quote_symbol = q.symbol order by p."until" desc offset 1 limit 1) as "prev_price",
-        (select p.price from asset_prices p where p.base_symbol = a.symbol and p.quote_symbol = q.symbol and now() >= p.since and now() < p."until" order by p."until" desc limit 1) as "price"
-        from assets a
-        join assets q on q.symbol = 'MNT'
-        where a."type" = 'stock'
-        order by a.symbol != 'AARD', a.symbol asc`
+        }[]>`select 
+        t.symbol,
+        t.display_symbol,
+        t."name",
+        t.prev_price,
+        (case when t.interval_price is not null then t.interval_price 
+        else (
+            select p.price from asset_prices p where p.base_symbol = t.symbol and p.quote_symbol = t.quote_symbol order by p."until" desc offset 0 limit 1
+        ) end) as "price"
+        from (
+            select
+            a.symbol as "symbol",
+            a.display_symbol,
+            q.symbol as "quote_symbol",
+            a."name" as "name",
+            (select p.price from asset_prices p where p.base_symbol = a.symbol and p.quote_symbol = q.symbol order by p."until" desc offset 1 limit 1) as "prev_price",
+            (select p.price from asset_prices p where p.base_symbol = a.symbol and p.quote_symbol = q.symbol and now() >= p.since and now() < p."until" order by p."until" desc limit 1) as "interval_price"
+            from assets a
+            join assets q on q.symbol = 'MNT'
+            where a."type" = 'stock'
+        ) as t
+        order by t.symbol != 'AARD', t.symbol asc`
         const formatted = assets.map((a) => ({
             symbol: a.symbol,
             name: a.name,
